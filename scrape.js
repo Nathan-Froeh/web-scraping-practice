e859048c41d923066319c50e9106bc7de15e3eb9
Nathan <nathan.froeh@gmail.com>
2019-08-12 17:11:25 -0600

    Add instagram test

--- scrape.js
+++ scrape.js
@@ -3,3 +3,23 @@
 // find closest article
 // console.log closest h2
 
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({ show: true });
+
+nightmare
+  .goto('https://www.instagram.com/')
+  .wait('article')
+  .evaluate(() => {
+    var nameNodes = document.querySelectorAll('h2');
+    var list = [].slice.call(nameNodes);
+    return list.map(function(node){
+      return node.innerText
+    });
+  })
+  .end()
+  .then(function (result) {
+    console.log(result);
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+  });
