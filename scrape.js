d4399808bb120b25f152ea0df8e8e0517656c4da
Nathan <nathan.froeh@gmail.com>
2019-08-12 18:56:01 -0600

    Add login to instagram

--- scrape.js
+++ scrape.js
@@ -3,23 +3,24 @@
 // find closest article
 // console.log closest h2
 
-var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true });
 
-nightmare
-  .goto('https://www.instagram.com/')
-  .wait('article')
-  .evaluate(() => {
-    var nameNodes = document.querySelectorAll('h2');
-    var list = [].slice.call(nameNodes);
-    return list.map(function(node){
-      return node.innerText
-    });
-  })
-  .end()
-  .then(function (result) {
-    console.log(result);
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error);
-  });
+const { Chromeless } = require('chromeless')
+
+async function run() {
+  const chromeless = new Chromeless()
+
+  const screenshot = await chromeless
+    .goto('https://www.instagram.com/')
+    .wait('button')
+    .click('button')
+    .type('nathan.froeh@gmail.com', 'input[name="email"]')
+    .type('', 'input[name="pass"]')
+    .click('input[name="login"]')
+    .click('input[value="Continue as Nathan"]')
+    .click('button[class="aOOlW   HoLwm "]')
+    .wait('', 50000)
+
+  await chromeless.end()
+}
+
+run().catch(console.error.bind(console))
